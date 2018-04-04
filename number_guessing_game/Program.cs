78e5196c1218750eeaa68f5b2509eb99221c0a73
Tiller <ctiller@proforma.com>
2018-04-04 09:16:13 -0400

    Create number guessing game

--- number_guessing_game/Program.cs
+++ number_guessing_game/Program.cs
@@ -10,6 +10,27 @@ namespace number_guessing_game
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+            int randNum = rnd.Next(100);
+            Console.WriteLine("Pick a number, between 1 and 100");
+            int answer = Convert.ToInt32(Console.ReadLine());
+            while (answer != randNum)
+            {
+                if (answer < randNum)
+                {
+                    Console.WriteLine("Higher!");
+                }
+                else
+                {
+                    Console.WriteLine("Lower!");
+                }
+                Console.WriteLine("Pick a number, between 1 and 100");
+                answer = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine($"Great Job! You guessed the correct number! It was {randNum}");
+            Console.ReadLine();
+
         }
     }
 }
